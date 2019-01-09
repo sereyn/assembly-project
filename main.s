52440d6014d57b2f07d9cf01666d92180bf0c034
Sereyn <clement.def.7@gmail.com>
2019-01-10 00:16:07 +0100

    empty

--- main.s
+++ main.s
@@ -1,6 +1,25 @@
+.data
+
+msg:
+	.ascii "loop\n"
+
 .text
 .global _start
 _start:
+	# save parameter in r8
+	pop %rdx
+	pop %rdx
+	pop %r8
+	
+loop:
+	mov $1, %rax
+	xor %rdi, %rdi
+	mov $msg, %rsi
+	mov $6, %rdx
+	syscall
+
+exit:
+	# exit(0)
 	mov $60, %rax
 	xor %rdi, %rdi
 	syscall
